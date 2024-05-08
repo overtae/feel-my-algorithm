0e06977a1fb59a1a0ba06757aba7d56547127a11
taeyoung <51291185+overtae@users.noreply.github.com>
2024-05-08 16:32:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun solution(num: Int): Int {
-        var number = num
+        var number = num.toLong()
         
         if (num == 1) return 0
         
-        for (i in 1..400) {
-            number = if (number % 2 == 0) number / 2 else number*3 + 1
-            if (number == 1) return i
+        for (i in 1..500) {
+            number = if (number % 2 == 0L) number / 2 else number*3 + 1
+            if (number == 1L) return i
         }
         
         return -1
