6c2fada25f563b898797097f26a0be89bec27d45
taeyoung <51291185+overtae@users.noreply.github.com>
2024-05-14 15:21:00 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 3.06 ms, Memory: 61.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+//         var answer: Int = 0
+        
+//         for (i in 0..t.length-p.length) {
+//             if (t.substring(i, i+p.length).toLong() <= p.toLong()) answer++
+//         }
+        
+//         return answer
+        
+        return (0..t.length-p.length).filter {
+            t.substring(it, it+p.length).toLong() <= p.toLong()
+        }.size
+    }
+}
