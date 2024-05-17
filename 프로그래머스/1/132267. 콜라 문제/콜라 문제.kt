e4abcb984879786fd7f265fd15ba1a2f061934a4
taeyoung <51291185+overtae@users.noreply.github.com>
2024-05-17 15:09:32 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
@@ -1,15 +1,13 @@
 class Solution {
-    fun solution(a: Int, b: Int, n: Int): Int {
-        var (div, mod) = Pair(n/a * b, n%a)
-        var answer: Int = div
-        
-        while ((div + mod) >= a) {
-            var notUsed = div + mod
-            div = notUsed/a * b
-            mod = notUsed%a
-            answer += div
-        }
-        
-        return answer
+  fun solution(a: Int, b: Int, n: Int): Int {
+    var answer: Int = 0
+    var bottles = n
+
+    while (bottles >= a) {
+	    answer += bottles/a * b
+      bottles = bottles/a*b + bottles%a
     }
+
+    return answer
+  }
 }
