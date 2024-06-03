a55ee3244b98f43ab6a00e8b8808f5cfb4b9c929
taeyoung <51291185+overtae@users.noreply.github.com>
2024-06-03 17:43:11 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 26.60 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
@@ -1,36 +1,16 @@
 class Solution {
     fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
-        var t = terms.map { 
-            var (k, v) = it.split(" ")
-            k to v.toInt()
-        }.toMap()
+        var t = terms.map { it.split(" ") }.associate { (a, b) -> a to b.toInt() }
+        var td = today.split(".").map(String::toInt).let { (y, m, d) -> 
+            (y * 12 * 28) + (m * 28) + d }
         var answer: IntArray = intArrayOf()
         
         privacies.mapIndexed { i, p ->
             var (date, type) = p.split(" ")
             var (y, m, d) = date.split(".").map { it!!.toInt() }
-            var term = t[type]!!
+            var term = (y * 12 * 28) + ((m + t[type]!!) * 28) + d - 1
             
-            y += term / 12
-            m += term % 12
-            d -= 1
-            
-            if (d == 0) {
-                m -= 1
-                d = 28
-            }
-            if (m == 0) {
-                y -= 1
-                m = 12
-            }
-            if (m > 12) {
-                y += 1
-                m %= 12
-            }
-            
-            var str = String.format("%d.%02d.%02d", y, m, d)
-            if (str < today) answer += i + 1
-            println(str)
+            if (term < td) answer += i + 1
         }
         
         return answer
