f45ee91069ae4890a52b13680cfb3bfdf7cf86c5
taeyoung <51291185+overtae@users.noreply.github.com>
2024-06-10 09:17:12 +0900

    [level 2] Title: 피보나치 수, Time: 4.38 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun fibo(x: Int, y: Int, cnt: Int): Int {
+        return if (cnt == 0) (x + y) % 1234567 else fibo(y % 1234567, x + y % 1234567, cnt - 1)
+    }
+    
+    fun solution(n: Int): Int {
+        return fibo(0, 1, n - 2)
+    }
+}
