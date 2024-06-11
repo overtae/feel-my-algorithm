51690d5af8f4362b3be9ebe1848932c7286004fb
taeyoung <51291185+overtae@users.noreply.github.com>
2024-06-11 10:53:46 +0900

    [level 2] Title: 카펫, Time: 1.83 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.kt
+++ 프로그래머스/2/42842. 카펫/카펫.kt
@@ -1,16 +1,11 @@
 class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
-        val all = brown + yellow
-        // val sq = Math.sqrt(all.toDouble()).toInt()
-        val divider = (1..all).filter { all % it == 0 }
-        var answer = intArrayOf()
-        
-        divider.map {
-            val (x, y) = all/it to it
-            if ((x - 2) * (y - 2) == yellow && x >= y)
-                answer = intArrayOf(x, y)
+        // (yellow/a * 2) + (a * 2) + 4 = brown
+        val divider = (1..yellow).filter { yellow % it == 0 }
+        val row = divider.first { 
+            brown == (yellow / it * 2) + it * 2 + 4
         }
         
-        return answer
+        return intArrayOf(yellow/row + 2, row + 2)
     }
 }
