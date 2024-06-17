fa7f80945c3fb1af7e9f85a9cde7baaf3432fa0d
taeyoung <51291185+overtae@users.noreply.github.com>
2024-06-17 20:10:44 +0900

    [level 2] Title: 괄호 회전하기, Time: 2.83 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -3,19 +3,19 @@ import java.util.Stack
 class Solution {
     fun solution(s: String): Int {
         var answer: Int = 0
-
-        s.indices.map { i ->
+        
+        for (i in s.indices) {
             val stack = Stack<Char>()
             val str = s.substring(i) + s.substring(0, i)
             
-            str.map {
+            for (c in str) {
                 val recent = if (stack.isNotEmpty()) stack.peek() else ' '
 
-                when (it) {
-                    ')' -> if (recent == '(') stack.pop() else stack.push(it)
-                    '}' -> if (recent == '{') stack.pop() else stack.push(it)
-                    ']' -> if (recent == '[') stack.pop() else stack.push(it)
-                    else -> stack.push(it)
+                when {
+                    (c == ')') && (recent == '(') -> stack.pop()
+                    (c == '}') && (recent == '{') -> stack.pop()
+                    (c == ']') && (recent == '[') -> stack.pop()
+                    else -> stack.push(c)
                 }
             }
             if (stack.isEmpty()) answer++
