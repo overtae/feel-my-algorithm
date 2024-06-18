aafa39d53392b1dc1667d69dc2f3d997eaefe2ea
taeyoung <51291185+overtae@users.noreply.github.com>
2024-06-18 17:47:23 +0900

    [level 2] Title: H-Index, Time: 2.64 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.kt
+++ 프로그래머스/2/42747. H－Index/H－Index.kt
@@ -1,9 +1,7 @@
 class Solution {
     fun solution(citations: IntArray): Int {
-        val sortedArr = citations.sortedDescending()
-        
         return (citations.size downTo 1).firstOrNull { h ->
-            sortedArr[h - 1] >= h
+            citations.count { it >= h } >= h
         } ?: 0
     }
 }
