abf345c4cf6fe9b28438f2e16d13d3144fa90a41
taeyoung <51291185+overtae@users.noreply.github.com>
2024-07-05 11:54:54 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.66 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -2,32 +2,23 @@ import java.util.*
 
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        val bridge: Queue<Int> = LinkedList(List(bridge_length) {0})
+        val trucks: Queue<Int> = LinkedList(truck_weights.toList())
         var answer = 0
-        var cur = 0
-        var weightSum = 0
-        val q = LinkedList<Int>() as Queue<Int>
         
-        while (true) {
-            answer += 1
+        while (bridge.isNotEmpty()) {
+            answer++
+            bridge.poll()
             
-            if (q.size  >= bridge_length) {
-                val item = q.poll()
-                if (item >= 0) {
-                    weightSum -= truck_weights[item]
-
-                    if (item  == truck_weights.size - 1) break
+            if (trucks.isNotEmpty()) {
+                if (bridge.sum()+trucks.peek() <= weight) {
+                    bridge.add(trucks.poll())
+                } else {
+                    bridge.add(0)
                 }
             }
-
-            if (cur < truck_weights.size && weightSum + truck_weights[cur] <= weight) {
-                q.add(cur)
-
-                weightSum += truck_weights[cur]
-                cur += 1
-            } else {
-                q.add(-1)
-            }
         }
+        
         return answer
     }
 }
