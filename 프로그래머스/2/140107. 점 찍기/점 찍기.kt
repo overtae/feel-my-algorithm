7f5318b774bee3f82c00764c7c029a5721248d16
taeyoung <51291185+overtae@users.noreply.github.com>
2024-07-18 09:47:03 +0900

    [level 2] Title: 점 찍기, Time: 24.09 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(k: Int, d: Int): Long {
+        // (0, 0)과 (x, y)의 거리 = d
+        // x^2 + y^2 = d^2
+        // y^2 = d^2 - x^2
+        return (0..d step k).fold(0L) { total, x ->
+            val y = d.toLong()*d - x.toLong()*x
+            total + Math.sqrt(y.toDouble()).toLong() / k + 1
+        }
+    }
+}
